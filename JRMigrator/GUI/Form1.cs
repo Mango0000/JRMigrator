d8779557cae21f872304ed1085b6df48716843ad
sarahp284 <prisad16@htl-kaindorf.at>
2019-12-12 14:34:26 +0100

    1st Form Layout

--- JRMigrator/GUI/Form1.cs
+++ JRMigrator/GUI/Form1.cs
@@ -17,5 +17,14 @@ namespace JRMigrator
             InitializeComponent();
         }
 
+        private void label2_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
