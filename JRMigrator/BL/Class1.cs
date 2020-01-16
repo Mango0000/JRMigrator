b7b2f7b9535dc5f6911d42274072954d574a6b04
sarahp284 <prisad16@htl-kaindorf.at>
2020-01-16 13:51:57 +0100

    -

--- JRMigrator/BL/Class1.cs
+++ JRMigrator/BL/Class1.cs
@@ -11,19 +11,37 @@ namespace JRMigrator.BL
     class Class1
     {
         private static DBStringBuilder dbfrom;
-        public static void start(String ip,String port,String databasename,String username,String password)
+        public static void start(String ip,String port,String databasename,String username,String password,int type)
         {
             DBType databasetype = DBType.CubridDB;
             dbfrom = new DBStringBuilder(databasetype, ip, port, databasename, username, password);
-            CubridSQLConnection csql = CubridSQLConnection.getConnection();
-            MSSQLConnection ms=MSSQLConnection.getConnection();
-            
-            csql.connectionString = dbfrom;
-
-            Console.Out.WriteLine(dbfrom.ToString());
+            OracleSQLConnection os=null;
+            CubridSQLConnection csql=null;
+            MSSQLConnection ms=null;
             try
             {
+            if (type == 1)
+            {
+               csql = CubridSQLConnection.getConnection();
+                csql.connectionString = dbfrom;
                 Console.Out.WriteLine(csql.OpenConnection() ? "erfolgreich" : "gescheitert");
+            }
+
+            if (type == 2)
+            {
+                ms = MSSQLConnection.getConnection();
+                ms.connectionString = dbfrom;
+                Console.Out.WriteLine(ms.OpenConnection() ? "erfolgreich" : "gescheitert");  
+            }
+            else
+            {
+                os=OracleSQLConnection.getConnection();
+                os.connectionString = dbfrom;
+                Console.Out.WriteLine(os.OpenConnection() ? "erfolgreich" : "gescheitert"); 
+            }
+            Console.Out.WriteLine(dbfrom.ToString());
+            
+
             }
             catch (Exception e)
             {
@@ -40,7 +58,9 @@ namespace JRMigrator.BL
                     MessageBox.Show("Invalid Port or Address");
                 }
             }
-            csql.CloseConnection();
+           // csql.CloseConnection();
+            //ms.CloseConnection();
+           // os.CloseConnection();
         }
     }
 }
