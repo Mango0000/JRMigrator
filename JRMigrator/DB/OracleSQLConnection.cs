baeb3f6fcfa1dc91b6c38cd161bb080ebf19b0fe
Hoemad <hoemad16@htlkaindorf.at>
2020-01-30 15:28:31 +0100

    Oracle ColumnInfo

--- JRMigrator/DB/OracleSQLConnection.cs
+++ JRMigrator/DB/OracleSQLConnection.cs
@@ -59,5 +59,76 @@ namespace JRMigrator.DB
             }
             return tableList;
         }
+
+        public List<TableInfo> getInfo(String tablename)
+        {
+            String sqlstring = "SELECT acc.column_name, utc.nullable, utc.data_type, ac.constraint_name, ac.constraint_type " +
+                               "FROM ALL_CONS_COLUMNS acc INNER JOIN USER_TAB_COLUMNS utc ON acc.column_name = utc.column_name " +
+                               "INNER JOIN ALL_CONSTRAINTS ac ON acc.constraint_name = ac.constraint_name " +
+                               "WHERE acc.owner = user AND acc.table_name = '"+tablename+"'; ";
+            OracleCommand omd = new OracleCommand(sqlstring, conn);
+            OracleDataReader reader = omd.ExecuteReader();
+            List<TableInfo> tbinf = new List<TableInfo>();
+            String column_name;
+            Boolean is_nullable;
+            String data_type;
+            String primary_key_name;
+            DataType datatype;
+            while (reader.Read())
+            {
+                column_name = reader.GetString(0);
+                is_nullable = reader.GetString(1) == "Y" ? true : false;
+                data_type = reader.GetString(2);
+                try
+                {
+                    primary_key_name = reader.GetString(3);
+                }
+                catch (System.Data.SqlTypes.SqlNullValueException)
+                {
+                    primary_key_name = null;
+                }
+                datatype = getDType(data_type);
+                tbinf.Add(new TableInfo(column_name, is_nullable, datatype, primary_key_name));
+            }
+            reader.Close();
+            return tbinf;
+        }
+
+        private DataType getDType(String data)
+        {
+            if (data == null)
+            {
+                return DataType.NULL;
+            }
+            else if (data.Equals("varchar"))
+            {
+                return DataType.VARCHAR;
+            }
+            else if (data.Equals("varchar2"))
+            {
+                return DataType.VARCHAR;
+            }
+            else if (data.Equals("int"))
+            {
+                return DataType.NUMBER;
+            }
+            else if (data.Equals("char"))
+            {
+                return DataType.CHAR;
+            }
+            else if (data.Equals("datetime"))
+            {
+                return DataType.DATETIME;
+            }
+            else if (data.Equals("date"))
+            {
+                return DataType.DATETIME;
+            }
+            else if (data.Equals("number"))
+            {
+                return DataType.NUMBER;
+            }
+            return DataType.NULL;
+        }
     }
 }
